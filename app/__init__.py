fe507e3fe2790990c40bcb4e1040d1ca7ed555c1
Ravi Tripathi <ravi89@uab.edu>
2018-06-04 22:59:46 -0500

    Adding configuration to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+import os
 
 # local imports
 from config import app_config
